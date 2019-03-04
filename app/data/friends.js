6faab009453596440704d35795859cba9f4a38a1
Ultraviolet <tierracook543@gmail.com>
2019-03-03 21:59:50 -0600

    html and js

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var matchArray = [
+	{
+		name: 'Kitty',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiKkpPjtufgAhWQVt8KHaCmCvgQjRx6BAgBEAU&url=https%3A%2F%2Fwww.meowingtons.com%2Fproducts%2Fkitty-cat-hippie-sunglasses&psig=AOvVaw3KzKygs_BITmwR8VsfuMsH&ust=1551752500211732',
+		scores: [
+			'1',
+			'2',
+			'5',
+			'1',
+			'4',
+			'4',
+			'2',
+			'1',
+			'1',
+			'4'
+		]
+	},
+	{
+		name: 'Woofers',
+		photo: 'https://goo.gl/images/RGNVHx',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Wolverine',
+		photo: 'https://goo.gl/images/P1Xv2S',
+		scores: [
+			'3',
+			'3',
+			'5',
+			'2',
+			'2',
+			'5',
+			'5',
+			'3',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Pam',
+		photo: 'https://goo.gl/images/fHtNcK',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Morty',
+		photo: 'https://goo.gl/images/fGWWnE',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Bananas',
+		photo: 'https://goo.gl/images/yfNdnL',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Keanu',
+		photo: 'https://goo.gl/images/dBkVLU',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Peter',
+		photo: 'https://goo.gl/images/CFrDjU',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jack',
+		photo: 'https://goo.gl/images/9v1pvs',
+		scores: [
+			'3',
+			'3',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Isaac',
+		photo: 'https://goo.gl/images/6TnD5T',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = matchArray;
