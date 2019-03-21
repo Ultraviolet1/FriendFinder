2759c2f20380090f2f9a40992870511bdea6fe3d
Ultraviolet <tierracook543@gmail.com>
2019-03-20 20:43:25 -0500

    PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 // Configure the Express application
 var app = express();
-var PORT = 7070;
+var PORT = process.env.PORT || 7070;
 
 // Expose the public directory to access CSS files
 app.use(express.static(path.join(__dirname, './app/public')));
